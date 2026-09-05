07a5218bfba54d360b9fc4cc27d646091575c1d9
she3b9620 <she3b9620@gmail.com>
2026-09-05 06:06:47 +0300

    style: load public profile styles

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -18,6 +18,7 @@ import "./ravine-sidebar.css";
 import "./ravine-ui-shell-polish.css";
 import "./ravine-search-font-lock.css";
 import "./ravine-home-account-sidebar-final.css";
+import "./ravine-public-profile.css";
 
 const locales = ["ar", "en"] as const;
 type Locale = (typeof locales)[number];
