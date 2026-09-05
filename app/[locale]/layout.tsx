ad3ec7a34e1303004a601b5815f4a4def4c28b4a
she3b9620 <she3b9620@gmail.com>
2026-09-05 04:36:06 +0300

    fix: load final mixed-script font lock last

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -11,6 +11,7 @@ import "./ravine-typography-search-final.css";
 import "./ravine-font-authority.css";
 import "./ravine-script-font-lock.css";
 import "./ravine-controls.css";
+import "./ravine-final-font-lock.css";
 
 const locales = ["ar", "en"] as const;
 type Locale = (typeof locales)[number];
