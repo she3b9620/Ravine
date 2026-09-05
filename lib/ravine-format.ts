f4aeabe3c2e7a35e031666298ba3962b5fecd6cf
she3b9620 <she3b9620@gmail.com>
2026-09-05 05:22:13 +0300

    fix: standardize identity digits

---
+++ lib/ravine-format.ts
@@ -0,0 +1,13 @@
+const ARABIC_DIGITS = "٠١٢٣٤٥٦٧٨٩";
+const PERSIAN_DIGITS = "۰۱۲۳۴۵۶۷۸۹";
+const WESTERN_DIGITS = "0123456789";
+
+export function westernDigits(value: string | number | null | undefined) {
+  return String(value ?? "")
+    .replace(/[٠-٩]/g, (digit) => WESTERN_DIGITS[ARABIC_DIGITS.indexOf(digit)])
+    .replace(/[۰-۹]/g, (digit) => WESTERN_DIGITS[PERSIAN_DIGITS.indexOf(digit)]);
+}
+
+export function ravineNumber(value: number | null | undefined) {
+  return new Intl.NumberFormat("en-US", { useGrouping: true }).format(Number(value ?? 0));
+}
