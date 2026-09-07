242418f846fe48d671f6368ac27b5393f6b6fe1a
she3b9620 <she3b9620@gmail.com>
2026-09-07 06:25:06 +0300

    style: wire creator identity switcher styles into locale shell

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -55,6 +55,7 @@ import "./ravine-final-batch-polish.css";
 import "./ravine-light-spacing-finish.css";
 import "./ravine-final-micro-polish.css";
 import "./ravine-radio.css";
+import "./ravine-creator-identity.css";
 
 const locales = ["ar", "en"] as const;
 type Locale = (typeof locales)[number];
